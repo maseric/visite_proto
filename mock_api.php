47e9a749b97db22304ebf4f78616f1e43f4cffc8
maseric <maseric@users.noreply.github.com>
2018-06-07 09:30:30 +0200

    Ajout petit favicon

--- mock_api.php
+++ mock_api.php
@@ -1,5 +1,5 @@
 <?php
 // script PHP pipeau pour envoyer une array de visteurs (chaque visiteur est un objet JSON)
 header('Content-type:application/json;charset=utf-8');
-echo '[{"id":1,"nom":"valjean","prenom":"jean","dateNaiss":"28/01/2009","nationalite":"1","entreprise":{"raisonSociale":"miserables inc","ville":"toulon","CP":"83000","pays":"1","adresse":"22 rue des putes","PJ":""}},{"id":2,"nom":"dupond moretti","prenom":"eric","dateNaiss":"06/01/2009","nationalite":"1","entreprise":{"raisonSociale":"raccroc inc","ville":"paris","CP":"75016","pays":"1","adresse":"1 rue du pdt coty","PJ":""}}]';
+echo '[{"id":1,"nom":"valjean","prenom":"jean","dateNaiss":"28/01/2009","nationalite":"1","entreprise":{"raisonSociale":"miserables inc","ville":"toulon","CP":"83000","pays":"1","adresse":"22 rue des poulpes","PJ":""}},{"id":2,"nom":"dupond moretti","prenom":"eric","dateNaiss":"06/01/2009","nationalite":"1","entreprise":{"raisonSociale":"raccroc inc","ville":"paris","CP":"75016","pays":"1","adresse":"1 rue du pdt coty","PJ":""}}]';
 ?>
